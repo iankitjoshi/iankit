92f2881355fe95a0f8be58f7d3cf9d42828ec0b6
Ankit Joshi <ankitkumarjoshi123@gmail.com>
2020-05-14 14:12:49 +0530

    logo

--- src/Components/Homes.js
+++ src/Components/Homes.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const img = require('../Assets/Homes.jpg')
+const img1 = require('../Assets/ankit1.png')
 
 function Homes(){
     return(
@@ -8,6 +9,7 @@ function Homes(){
             <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@700&display=swap" rel="stylesheet" />
             <div className="containers">
                 <img src={img} className="home-img" />
+                <img src={img1} className="logo-img" width="100" />
                 <h1 className="centered">Hi, i'm Ankit. Full Stack Developer, I design & build user interfaces. </h1>
             </div>
 
